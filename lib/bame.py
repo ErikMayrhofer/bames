bf18b8e077b893da4e9e94eafa29c84f79ef3832
Erik Mayrhofer <obyoxar@gmail.com>
2021-07-03 00:04:43 +0200

    bame scenes

--- lib/bame.py
+++ lib/bame.py
@@ -1,39 +1,42 @@
-from typing import Type
+from typing import Type, Any
 import pygame
+from lib.util.keyframes import Keyframes
 
 class TickContext:
     fps: float
     delta_ms: int
+    screen: Any
+
+class SplashScene:
+    def __init__(self):
+        self.splash_img = pygame.image.load("img/Logo.jpg")
+        self.frames = Keyframes([(0, 0), (0.5, 255), (1.3, 255), (1.5, 0)])
+
+    def tick(self, context: TickContext) -> bool:
+        self.frames.advance(context.delta_ms/1000)
+        val = self.frames.value()
+        context.screen.fill((255,255,255))
+        context.screen.blit(self.splash_img, (0, 0, 640, 480))
+        context.screen.fill((val, val, val),special_flags=pygame.BLEND_MULT)
+        return self.frames.done()
+
 
 class Bame:
     def __init__(self, classname: Type):
         self.game_instance = classname()
         self.running = False
-
-    
+        self.scenes = [SplashScene(), self.game_instance]
 
     def run(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((640, 480))
-
-
-        splash_img = pygame.image.load("img/Logo.jpg")
-
-        for i in range(255):
-            self.screen.fill((255,255,255))
-            self.screen.blit(splash_img, (0, 0, 640, 480))
-            self.screen.fill((i, i, i),special_flags=pygame.BLEND_MULT)
-            pygame.display.flip()
-            pygame.time.wait(1)
-        pygame.time.wait(100)
-        for i in reversed(range(255)):
-            self.screen.fill((255,255,255))
-            self.screen.blit(splash_img, (0, 0, 640, 480))
-            self.screen.fill((i, i, i),special_flags=pygame.BLEND_MULT)
-            pygame.display.flip()
-            pygame.time.wait(1)
+        self.screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
 
         self.start_loop()
+
+    def next_scene(self):
+        self.scenes.pop(0)
+        if len(self.scenes) == 0:
+            self.running = False
 
     def start_loop(self):
         clock = pygame.time.Clock()
@@ -44,18 +47,27 @@ class Bame:
             context = TickContext()
             context.fps = 1000/delta_t
             context.delta_ms = delta_t
+            context.screen = self.screen
 
             self.screen.fill((0, 0, 0)) 
-
-            for event in pygame.event.get():
-                self.handle_event(event)
             
-            self.game_instance.tick(context)
+            self.handle_events()
+            
+            next_scene = self.scenes[0].tick(context)
+            if next_scene:
+                self.next_scene()
 
             pygame.display.flip()
 
         print("Bye!")
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            self.handle_event(event)
             
     def handle_event(self, event):
+        print(f"EventType {event}")
         if event.type == pygame.QUIT:
             self.running = False
+        if event.type == pygame.WINDOWRESIZED:
+            pygame.display.update()
